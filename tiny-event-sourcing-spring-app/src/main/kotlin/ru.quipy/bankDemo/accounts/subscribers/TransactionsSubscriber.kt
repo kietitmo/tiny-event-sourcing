c675218434b123ed14d588f036ba61533d411263
kietitmo <kietitmo@gmail.com>
2023-12-03 12:35:48 +0300

    ft: adding saga for transfer-account (base on sagaDemo)

--- tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/accounts/subscribers/TransactionsSubscriber.kt
+++ tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/accounts/subscribers/TransactionsSubscriber.kt
@@ -4,29 +4,34 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import ru.quipy.bankDemo.accounts.api.AccountAggregate
-import ru.quipy.bankDemo.transfers.api.TransferTransactionAggregate
-import ru.quipy.bankDemo.transfers.api.TransferTransactionCreatedEvent
+import ru.quipy.bankDemo.transfers.api.*
 import ru.quipy.bankDemo.accounts.logic.Account
-import ru.quipy.bankDemo.transfers.api.TransactionConfirmedEvent
-import ru.quipy.core.EventSourcingService
 import ru.quipy.streams.AggregateSubscriptionsManager
+import ru.quipy.core.EventSourcingService
+import ru.quipy.saga.SagaManager
 import java.util.*
 import javax.annotation.PostConstruct
 
 @Component
 class TransactionsSubscriber(
     private val subscriptionsManager: AggregateSubscriptionsManager,
-    private val accountEsService: EventSourcingService<UUID, AccountAggregate, Account>
+    private val accountEsService: EventSourcingService<UUID, AccountAggregate, Account>,
+    private val sagaManager: SagaManager
 ) {
     private val logger: Logger = LoggerFactory.getLogger(TransactionsSubscriber::class.java)
+    val bankSagaName = "BANK_OPERATION"
 
     @PostConstruct
     fun init() {
         subscriptionsManager.createSubscriber(TransferTransactionAggregate::class, "accounts::transaction-processing-subscriber") {
             `when`(TransferTransactionCreatedEvent::class) { event ->
                 logger.info("Got transaction to process: $event")
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "perform transfer")
+                    .sagaContext
 
-                val transactionOutcome1 = accountEsService.update(event.sourceAccountId) { // todo sukhoa idempotence!
+                val transactionOutcome1 = accountEsService.update(event.sourceAccountId, sagaContext) { // todo sukhoa idempotence!
                     it.performTransferFrom(
                         event.sourceBankAccountId,
                         event.transferId,
@@ -34,7 +39,7 @@ class TransactionsSubscriber(
                     )
                 }
 
-                val transactionOutcome2 = accountEsService.update(event.destinationAccountId) { // todo sukhoa idempotence!
+                val transactionOutcome2 = accountEsService.update(event.destinationAccountId, sagaContext) { // todo sukhoa idempotence!
                     it.performTransferTo(
                         event.destinationBankAccountId,
                         event.transferId,
@@ -44,20 +49,44 @@ class TransactionsSubscriber(
 
                 logger.info("Transaction: ${event.transferId}. Outcomes: $transactionOutcome1, $transactionOutcome2")
             }
+
             `when`(TransactionConfirmedEvent::class) { event ->
                 logger.info("Got transaction confirmed event: $event")
 
-                val transactionOutcome1 = accountEsService.update(event.sourceAccountId) { // todo sukhoa idempotence!
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "process transfer")
+                    .sagaContext
+
+                val transactionOutcome1 = accountEsService.update(event.sourceAccountId, sagaContext) { // todo sukhoa idempotence!
                     it.processPendingTransaction(event.sourceBankAccountId, event.transferId)
                 }
 
-                val transactionOutcome2 = accountEsService.update(event.destinationAccountId) { // todo sukhoa idempotence!
+                val transactionOutcome2 = accountEsService.update(event.destinationAccountId, sagaContext) { // todo sukhoa idempotence!
                     it.processPendingTransaction(event.destinationBankAccountId, event.transferId)
                 }
 
                 logger.info("Transaction: ${event.transferId}. Outcomes: $transactionOutcome1, $transactionOutcome2")
             }
-            // todo sukhoa bank account deleted event
+
+            `when`(TransactionNotConfirmedEvent::class) { event ->
+                logger.info("Got transaction not confirmed event: $event")
+
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "rollback transfer")
+                    .sagaContext
+
+                val transactionOutcome1 = accountEsService.update(event.sourceAccountId, sagaContext) { // todo sukhoa idempotence!
+                    it.rollbackPendingTransaction(event.sourceBankAccountId, event.transferId)
+                }
+
+                val transactionOutcome2 = accountEsService.update(event.destinationAccountId, sagaContext) { // todo sukhoa idempotence!
+                    it.rollbackPendingTransaction(event.destinationBankAccountId, event.transferId)
+                }
+
+                logger.info("Transaction: ${event.transferId}. Rollbacks: $transactionOutcome1, $transactionOutcome2")
+            }
         }
     }
 }
--- tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/transfers/subscribers/BankAccountsSubscriber.kt
+++ tiny-event-sourcing-spring-app/src/main/kotlin/ru.quipy/bankDemo/transfers/subscribers/BankAccountsSubscriber.kt
@@ -10,36 +10,62 @@ import ru.quipy.bankDemo.accounts.logic.Account
 import ru.quipy.bankDemo.transfers.logic.TransferTransaction
 import ru.quipy.core.EventSourcingService
 import ru.quipy.streams.AggregateSubscriptionsManager
+import ru.quipy.saga.SagaManager
 import java.util.*
 import javax.annotation.PostConstruct
 
 @Component
 class BankAccountsSubscriber(
     private val subscriptionsManager: AggregateSubscriptionsManager,
-    private val transactionEsService: EventSourcingService<UUID, TransferTransactionAggregate, TransferTransaction>
+    private val transactionEsService: EventSourcingService<UUID, TransferTransactionAggregate, TransferTransaction>,
+    private val sagaManager: SagaManager
 ) {
     private val logger: Logger = LoggerFactory.getLogger(BankAccountsSubscriber::class.java)
+    val bankSagaName = "BANK_OPERATION"
 
     @PostConstruct
     fun init() {
         subscriptionsManager.createSubscriber(AccountAggregate::class, "transactions::bank-accounts-subscriber") {
             `when`(TransferTransactionAcceptedEvent::class) { event ->
-                transactionEsService.update(event.transactionId) {
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "process ParticipantAccept")
+                    .sagaContext
+
+                transactionEsService.update(event.transactionId, sagaContext) {
                     it.processParticipantAccept(event.bankAccountId)
                 }
             }
+
             `when`(TransferTransactionDeclinedEvent::class) { event ->
-                transactionEsService.update(event.transactionId) {
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "process ParticipantDecline")
+                    .sagaContext
+
+                transactionEsService.update(event.transactionId, sagaContext) {
                     it.processParticipantDecline(event.bankAccountId)
                 }
             }
+
             `when`(TransferTransactionProcessedEvent::class) { event ->
-                transactionEsService.update(event.transactionId) {
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "finish transfer")
+                    .sagaContext
+
+                transactionEsService.update(event.transactionId, sagaContext) {
                     it.participantCommitted(event.bankAccountId)
                 }
             }
+
             `when`(TransferTransactionRollbackedEvent::class) { event ->
-                transactionEsService.update(event.transactionId) {
+                val sagaContext = sagaManager
+                    .withContextGiven(event.sagaContext)
+                    .performSagaStep(bankSagaName, "rollback transfer")
+                    .sagaContext
+
+                transactionEsService.update(event.transactionId, sagaContext) {
                     it.participantRollbacked(event.bankAccountId)
                 }
             }
